0ac5d71224ea859156d5e7cfe007ecb118eaf2f1
Kanaga-Vasanth1 <98308539+Kanaga-Vasanth1@users.noreply.github.com>
2022-01-24 13:20:32 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Web.Mvc;
+using NameApplication.Models;
+
+namespace NameApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveStudent(Student student)
+        {
+
+            string FirstName = student.FirstName;
+            string LastName = student.LastName;
+            Student obj = new Student();
+          
+            return Json(obj, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace NameApplication.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
